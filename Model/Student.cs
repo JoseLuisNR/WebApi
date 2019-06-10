2c371b63e6c09c61eddcb8f74e2558e8b31a3e2a
josel <20175310@itla.edu.do>
2019-06-10 03:29:59 -0400

    Primer commit con las 3 clases creadas

---
+++ Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String Bio { get; set; } 
+    }
+}
---
+++ Persistence/StudenDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class StudenDbContext: DbContext
+    
+    {
+        public DbSet<Student>Student { get; set; }
+        public StudenDbContext(DbContextOptions<StudenDbContext> options)
+            : base(options)
+        { }
+
+    }
+}
